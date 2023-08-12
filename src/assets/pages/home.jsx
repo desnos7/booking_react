d85a19c41dc9629beafca2bef118b88df8cad158
desnos7 <beugredesnosjeremie@gmail.com>
2023-08-12 12:09:58 +0000

    book

---
+++ src/assets/pages/home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import  Navbar  from "../Components/navbar.jsx";
+
+function Home(){
+    
+    return (
+        <>
+        <Navbar/>
+        </>
+      
+    )
+ 
+}
+
+export default Home
